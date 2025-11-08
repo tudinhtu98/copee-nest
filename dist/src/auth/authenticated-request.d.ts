ed536a86b6f69e0c0c04332759cf874f669d198b
tudinhtu98 <tudinhtu98@gmail.com>
2025-11-08 16:17:35 +0700

    chore: remove dist folder

--- dist/src/auth/authenticated-request.d.ts
+++
@@ -1,9 +0,0 @@
-import { Request } from 'express';
-import { UserRole } from '@prisma/client';
-export interface AuthenticatedRequest extends Request {
-    user: {
-        userId: string;
-        role: UserRole;
-        username?: string;
-    };
-}
